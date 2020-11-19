41e3559b8d3c2b06817b05f311d5164cefbf4ddf
Alphinkp <74713468+Alphinkp@users.noreply.github.com>
2020-11-19 16:55:20 +0530

    Add files via upload

---
+++ Question 4.py
@@ -0,0 +1,11 @@
+year=int(input("Enter the year"))
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("The year",year,"is a Leapyear")
+        else:
+            print("The year",year,"is not a Leapyear")
+    else:
+        print("The year",year,"is a Leapyear")
+else:
+    print("The year",year,"is not a Leapyear")
